606519f9fc5732bddac773894d22d65dbebc7cef
Anthony Petrov <beavean@gmail.com>
2022-06-11 18:15:57 +0300

    Label and Outlets added

---
+++ Names to Faces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Names to Faces
+//
+//  Created by Beavean on 11.06.2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var name: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+}
